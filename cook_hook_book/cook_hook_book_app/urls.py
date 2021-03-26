ab4141e2b75056e78e78a8bd4b69ce4ddaebaffc
Griffin Wimminger <impendingdelusion@gmail.com>
2021-03-25 17:26:01 -0700

    images now work

--- cook_hook_book/cook_hook_book_app/urls.py
+++ cook_hook_book/cook_hook_book_app/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'cook_hook_book_app'
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('new_post', views.new_post, name='new_post'),
-]
+    path('new_card/<int:post_id>', views.new_card, name='new_card'),
+    path('create_post', views.create_post, name='create_post'),
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- cook_hook_book/cook_hook_book_app/views.py
+++ cook_hook_book/cook_hook_book_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Card, Image, Post
 
@@ -10,4 +12,20 @@ def home(request):
     return render(request, 'home.html', context)
 
 def new_post(request):
-    
+    new_post = Post.objects.create()
+    context = {
+        'new_post': new_post,
+    }
+    return render(request, 'new_post.html', context)
+
+def new_card(request, post_id):
+    post_got = get_object_or_404(Post,pk=post_id)
+    Card.objects.create(post=post_got)
+    context = {
+        'new_post': new_post,
+    }
+    return HttpResponseRedirect(reverse('cook_hook_book_app:new_post'), context)
+
+
+def create_post(request):
+    return HttpResponseRedirect(reverse('cook_hook_book_app:home'))
